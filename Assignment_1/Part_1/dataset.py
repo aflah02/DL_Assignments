3f734f5afd78e971c463121e7925920f51188c87
stealth <generalom1234@gmail.com>
2023-02-11 00:38:19 +0530

    final changes for a1q1, probably

--- Assignment_1/Part_1/dataset.py
+++ Assignment_1/Part_1/dataset.py
@@ -8,7 +8,7 @@ class BooleanDataset:
         if self.boolean_op == 'AND' or self.boolean_op == 'OR' or self.boolean_op == 'XOR':
             self.data = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]]    # x0, x1, x2
         elif self.boolean_op == 'NOT':
-            self.data = [[1,0], [1,1]]                    # x0, x1
+            self.data = [[1,0,0], [1,1,0]]
         for i in range(len(self.data)):
             if self.boolean_op == 'AND':
                 self.labels.append(self.data[i][1] and self.data[i][2])
@@ -17,6 +17,6 @@ class BooleanDataset:
             elif self.boolean_op == 'XOR':
                 self.labels.append(self.data[i][1] ^ self.data[i][2])
             elif self.boolean_op == 'NOT':
-                self.labels.append(not self.data[i])
+                self.labels.append(not self.data[i][1])
         
         self.labels = [-1 if label==0 else label for label in self.labels]
