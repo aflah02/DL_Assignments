07aaaa8bc01c5e03da3eed2cfba87d55c3c0beac
stealth <generalom1234@gmail.com>
2023-02-23 22:32:15 +0530

    resnet tuned

--- Assignment_2/Part_3/cnn.py
+++ Assignment_2/Part_3/cnn.py
@@ -12,23 +12,29 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
-        self.conv1 = nn.LazyConv2d(out_channels=10, kernel_size=5)
+        self.conv1 = nn.LazyConv2d(out_channels=10, kernel_size=5, padding=2)
         self.conv2 = nn.LazyConv2d(out_channels=20, kernel_size=2)
 
         self.fc1 = nn.LazyLinear(out_features=50)
         self.out = nn.LazyLinear(out_features=10)
 
     def forward(self, inputs):
-        conv_output = self.conv1(inputs)
-        pooled_output = F.max_pool2d(conv_output, kernel_size=2, stride=2)
-        activation_output = F.relu(pooled_output)
+        conv_output = self.conv1(inputs)    # 28 x 28
+        # print(conv_output.shape)
+        pooled_output = F.max_pool2d(conv_output, kernel_size=2, padding = 1, stride=1)    # 29 x 29
+        # print(pooled_output.shape)
+        activation_output_1 = F.relu(pooled_output)            # 29 x 29
 
-        conv_output = self.conv2(activation_output)
-        pooled_output = F.max_pool2d(conv_output, kernel_size=4, stride=2)
-        activation_output = F.relu(pooled_output)
+        conv_output = self.conv2(activation_output_1)   # 28 x 28
+        pooled_output_2 = F.max_pool2d(conv_output, kernel_size=2, padding = 1, stride=1)
+        # print(conv_output.shape)
 
-        linear_input = activation_output.reshape(-1, 320)
+        pooled_output = F.max_pool2d(pooled_output_2, kernel_size=2, stride=2)
+
+        activation_output_2 = F.relu(pooled_output)
+        linear_input = activation_output_2.reshape(-1, activation_output_2.shape[-1]*activation_output_2.shape[-2]*activation_output_2.shape[-3])
         linear_output = self.fc1(linear_input)
+
         activation_output = F.relu(linear_output)
 
         logits = self.out(activation_output)
